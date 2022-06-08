c603b0f7541f1cc42f39c2b0ad8d262e5869fc21
JoshDT900 <DJTroutman900@gmail.com>
2022-06-08 10:38:58 -0400

    Remove redundancies

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 let gridBox = document.querySelector('.grid-container');
 
-let gridElements = document.createElement('div');
-gridElements.setAttribute('class', 'gridBox');
-
-
 let gridBuilder = (userInput) => {
   for (let i = 0; i < (userInput * userInput); i++) {
     gridBox.appendChild(document.createElement('div')).setAttribute('class', 'gridBox');
@@ -12,4 +8,4 @@ let gridBuilder = (userInput) => {
   gridBox.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
 }
 
-gridBuilder(50);
+gridBuilder(8);
