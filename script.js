e2336f21d0cfb9eca6f7b3d8876cc50193b34627
JoshDT900 <DJTroutman900@gmail.com>
2022-06-08 13:37:46 -0400

    Add coloring functionality

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 let gridBox = document.querySelector('.grid-container');
+let userInput = 16;
+let boxArr = [];
+let colorChoice = "red";
 
 let gridBuilder = (userInput) => {
   for (let i = 0; i < (userInput * userInput); i++) {
-    gridBox.appendChild(document.createElement('div')).setAttribute('class', 'gridBox');
+    gridBox.appendChild(document.createElement('div')).setAttribute('class', `gridBox${i}`);
   }
 
-  gridBox.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
+  return gridBox.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
 }
 
-gridBuilder(80);
+gridBuilder(userInput);
+
+for (let i = 0; i < (userInput * userInput); i++) {
+  boxArr.push(document.querySelector(`.gridBox${i}`))
+}
+
+let changeColor = (e) => {  
+  e.target.style = `background-color: ${colorChoice}`;
+}
+
+boxArr.forEach(box => box.addEventListener('mouseover', changeColor))
+
